3d8155a5c6fafd614eb98586d6c5b6ad8013929c
reh42 <reharbison42@gmail.com>
2021-02-08 19:59:44 -0600

    completed form

--- src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -9,7 +9,17 @@ public class SkillsController {
     @GetMapping("form")
     @ResponseBody
     public String getForm(){
-        return "<html><body> name:<input name ='name' type = 'text'>";
+        return "<html><body> <form action='/form' method=post> name:<input name ='name' type = 'text'>" +
+                "<br>" +
+                "My Favorite Language: <br>" +
+                "<select name=languageOne><option value=Java >Java</option><option value=JavaScript>JavaScript</option><option value=Ruby>Ruby</option></select>" +
+                "My Second Favorite Language: <br>" +
+                "<select name=languageTwo><option value=Java >Java</option><option value=JavaScript>JavaScript</option><option value=Ruby>Ruby</option></select>" +
+                "My Third Favorite Language: <br>" +
+                "<select name=languageThree><option value=Java >Java</option><option value=JavaScript>JavaScript</option><option value=Ruby>Ruby</option></select>" +
+                "<input type=submit> </form> </body> </html>"
+
+                ;
     }
 
 }
