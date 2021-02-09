8be445a043e04fb11ea57dacd723cd1a3dcb4a6e
Dylan Mullins <dylanemullins92@gmail.com>
2021-02-08 20:13:27 -0600

    "Added postForm method and cleaned HTML"

--- src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -2,6 +2,8 @@ package org.launchcode.skillstracker.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -9,17 +11,28 @@ public class SkillsController {
     @GetMapping("form")
     @ResponseBody
     public String getForm(){
-        return "<html><body> <form action='/form' method=post> name:<input name ='name' type = 'text'>" +
+        return "<html><body> <form action='/form' method=post> name:<br><input name ='name' type = 'text'>" +
                 "<br>" +
                 "My Favorite Language: <br>" +
                 "<select name=languageOne><option value=Java >Java</option><option value=JavaScript>JavaScript</option><option value=Ruby>Ruby</option></select>" +
-                "My Second Favorite Language: <br>" +
+                "<br>My Second Favorite Language: <br>" +
                 "<select name=languageTwo><option value=Java >Java</option><option value=JavaScript>JavaScript</option><option value=Ruby>Ruby</option></select>" +
-                "My Third Favorite Language: <br>" +
+                "<br>My Third Favorite Language: <br>" +
                 "<select name=languageThree><option value=Java >Java</option><option value=JavaScript>JavaScript</option><option value=Ruby>Ruby</option></select>" +
-                "<input type=submit> </form> </body> </html>"
+                "<br><input type=submit> </form> </body> </html>";
+    }
 
-                ;
+    @PostMapping("form")
+    @ResponseBody
+    public String postForm(@RequestParam String name,
+                           @RequestParam String languageOne,
+                           @RequestParam String languageTwo,
+                           @RequestParam String languageThree) {
+        return "<html><body><h1>" + name + "</h1>" +
+                "<ol><li>" + languageOne + "</li>" +
+                "<li>" + languageTwo + "</li>" +
+                "<li>" + languageThree + "</li></ol></body></html>";
+
     }
 
 }
